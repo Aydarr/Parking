ef9bf77874a603114a560ca8f4d52ad8d5e85eef
Айдар Сайфутдинов <stud0000192492@study.utmn.ru>
2020-06-27 06:23:31 +0500

    4check

--- app/src/main/java/com/example/parking/MainActivity.kt
+++ app/src/main/java/com/example/parking/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.parking
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +15,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var firstName:String
+        var carId:String
+        val extras = getIntent().extras
+        if (extras != null) {
+            firstName = extras.getString("firstName").toString()
+            carId = extras.getString("carId").toString()
+        }
+        val db=FirebaseFirestore.getInstance()
+        val parkedCar = hashMapOf(
+            //"name" to firstName,
+            "time" to "2"
+        )
 
+        db.collection("cities").document("")
+            .set(parkedCar as Map<String, Any>)
+            .addOnSuccessListener { Log.d("Success", "DocumentSnapshot successfully written!") }
+            .addOnFailureListener { e -> Log.w("Fail", "Error writing document", e) }
 
         button.setOnClickListener {
             val intent = Intent(this,ParkingActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
+
         }
         button2.setOnClickListener {
             val myintent = Intent(this,Main2Activity::class.java)
